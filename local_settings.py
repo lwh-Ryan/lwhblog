1d754ebe570a8edb587cf6caf568963f4773605f
Ryan <543764210@qq.com>
2016-09-21 15:14:45 +0800

    first_commit

---
+++ local_settings.py
@@ -0,0 +1,16 @@
+# project setting
+debug = True
+storage = 'qiniu'
+
+# DB settings
+DB_HOST = '127.0.0.1'
+DB_PORT = 3306
+DB_NAME = 'lwh_blog'
+DB_USER = 'root' 
+DB_PASSWORD = '123456' 
+
+# QiNiu storage settings
+QN_ACCESS_KEY = 'HqwTuQlC2jI-2YGiDZTnyQBTFA-10xMwVHYSKmnr' 
+QN_SECRET_KEY = 'lLEFjpBtWGqOJtDLPNMFc204z7Z7dka1-jqkFO6T' 
+QN_BUCKET_NAME = 'lwhryan-blog'
+QN_BUCKET_DOMAIN = 'odblm5gk0.bkt.clouddn.com'
